193b6982603573db360ed6c4571b5bf0d7724e34
Pakphom Promsean <pakphom.p@extend-it-resource.com>
2024-05-30 17:13:57 +0700

    Added

--- src/main/java/com/go/backend_crud/service/AccountService.java
+++ src/main/java/com/go/backend_crud/service/AccountService.java
@@ -1,11 +1,13 @@
 package com.go.backend_crud.service;
 
+import com.go.backend_crud.exception.ErrorResponse;
 import com.go.backend_crud.model.auth.dto.RegisterDto;
 import com.go.backend_crud.model.entity.AccountEntity;
 import com.go.backend_crud.repository.AccountRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Slf4j
